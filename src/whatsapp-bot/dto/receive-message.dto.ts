46fdd6cff8d6a57c99118646158572a52f30792c
Yeisser Cortez <yeisserc@gmail.com>
2026-08-09 22:30:35 -0400

    Working meta webhook

--- src/whatsapp-bot/dto/receive-message.dto.ts
+++ src/whatsapp-bot/dto/receive-message.dto.ts
@@ -1,7 +1,8 @@
 export class ReceiveMessageDto {
   phoneNumber!: string;
   message?: string;
-  mediaUrl?: string; // URL de la imagen/screenshot
-  mediaType?: string; // 'image', 'document', etc.
+  mediaUrl?: string;
+  mediaId?: string;
+  mediaType?: string;
   messageId?: string;
 }
